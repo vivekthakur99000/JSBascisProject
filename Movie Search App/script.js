39efcc0239b18bd8b6a0140b7b2fb77ee412d6e0
vivek thakur <vivekthakur99000@gmail.com>
2025-01-18 13:08:15 +0530

    IMDB Rating App

---
+++ Movie Search App/script.js
@@ -0,0 +1,19 @@
+
+const movieSearch = document.getElementById("movie-search").value
+const year = document.getElementById("year").value
+const search = document.getElementById("search")
+const poster = document.getElementById("movie-poster")
+const title = document.getElementById("title")
+const relaseYear = document.getElementById("rele-year")
+const lang = document.getElementById("language")
+const actors = document.getElementById("actors")
+const rating = document.getElementById("rating")
+const type = document.getElementById("type")
+
+const apikey = "cdcbcb85"
+
+const url = `http://www.omdbapi.com/?apikey=cdcbcb85&`
+
+function getMovieDetails(){
+
+}
